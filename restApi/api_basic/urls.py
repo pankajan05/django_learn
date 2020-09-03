c163f2d2ab793260d113fa730ecc71ba71d480a2
pankajan05 <pankajan05@gmail.com>
2020-09-03 13:03:28 +0530

    delete and get a particular element using id

--- restApi/api_basic/urls.py
+++ restApi/api_basic/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from api_basic.views import book_list
+from api_basic.views import book_list, book_detail
 
 urlpatterns = [
     path('book/', book_list),
+    path('detail/<int:pk>/', book_detail),
 ]
--- restApi/api_basic/views.py
+++ restApi/api_basic/views.py
@@ -20,3 +20,29 @@ def book_list(request):
             serializer.save()
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+def book_detail(request, pk):
+
+    try:
+        print(pk)
+        book = Book.objects.get(pk=pk)
+    except Book.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = BookSerializer(book)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = BookSerializer(book, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        book.delete()
+        return HttpResponse(status=204)
