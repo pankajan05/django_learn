eec1c4656f477728475404c4cf2fee944bff87d8
pankajan05 <pankajan05@gmail.com>
2020-09-03 08:44:32 +0530

    create a model book and register it in admin

--- restApi/api_basic/models.py
+++ restApi/api_basic/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    ISBN = models.CharField(max_length=10)
+    Title = models.CharField(max_length=200)
+    Author = models.CharField(max_length=200)
+    Date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.Title
